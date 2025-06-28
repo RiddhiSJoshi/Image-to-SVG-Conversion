3bba65a90990a8a970358ad323a5e428e4332316
Riddhi Joshi <riddhijoshi.enspirittech@gmail.com>
2025-06-28 23:47:43 +0530

    Facing an issue in loading the starVector model from hugging fface transformer.

--- Backend/app/model_utils.py
+++ Backend/app/model_utils.py
@@ -1,12 +1,26 @@
 # create a backend API that uses AI (StarVector model from Hugging Face) to automatically convert raster images (like PNG or JPEG) into SVG vector graphics.
-from transformers import AutoProcessor, AutoModelForVision2Seq
+from transformers import (
+    VisionEncoderDecoderProcessor,
+    SiglipImageProcessor,
+    AutoTokenizer,
+    AutoModelForVision2Seq,
+)
+# from transformers import AutoProcessor, AutoModelForVision2Seq
 from PIL import Image
 import torch
 import io
 
+model_id = "starvector/starvector-8b-im2svg"
 # Load StarVector model and processor (once)
-processor = AutoProcessor.from_pretrained("facebook/starvector")
-model = AutoModelForVision2Seq.from_pretrained("facebook/starvector")
+# processor = AutoProcessor.from_pretrained(model_id)
+# Correct way to load processor
+image_processor = SiglipImageProcessor.from_pretrained(model_id)
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+processor = VisionEncoderDecoderProcessor(
+    image_processor=image_processor,
+    tokenizer=tokenizer
+)
+model = AutoModelForVision2Seq.from_pretrained(model_id)
 
 def image_to_svg(image_bytes: bytes) -> str:
     try:
